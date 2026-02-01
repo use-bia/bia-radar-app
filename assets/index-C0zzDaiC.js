d2dbb53d11069a3a5f1916b274416af245337745
Caracol <rafaelfariasm@live.com>
2026-02-01 05:28:22 -0300

    Updates

--- assets/index-C0zzDaiC.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-D2GHs_wC.js";function n(){return o.jsx("div",{children:'Hello "/dashboard/guides/"!'})}export{n as component};
---
+++ assets/index-CfH_C-7L.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-DNiCU52m.js";function n(){return o.jsx("div",{children:'Hello "/dashboard/guides/"!'})}export{n as component};
