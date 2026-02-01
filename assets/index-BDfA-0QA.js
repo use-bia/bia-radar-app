cc7eb48905028bcf78a9c3380d9f87058c366dc5
Rafael <rafael@Caramelo.com>
2026-02-01 16:08:21 -0300

    Updates

--- assets/index-BDfA-0QA.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-CJAeo9Yg.js";function n(){return o.jsx("div",{children:'Hello "/dashboard/guides/"!'})}export{n as component};
---
+++ assets/index-BQcvdRX1.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-2NPPXlXg.js";function n(){return o.jsx("div",{children:'Hello "/dashboard/guides/"!'})}export{n as component};
