0edad106cf9ce25528790ef94589a6affe5fce80
Rafael <rafael@Caramelo.com>
2026-02-11 15:52:27 -0300

    added basic components on status page

--- src/routes/dashboard/status/index.tsx
+++ src/routes/dashboard/status/index.tsx
@@ -1,7 +1,7 @@
 import { GlassCard } from "@/components/GlassCard";
 import { Button, Card } from "@heroui/react";
 import { createFileRoute } from "@tanstack/react-router";
-import { Volume2Icon } from "lucide-react";
+import { BatteryIcon, ChevronRightIcon, CloudDownloadIcon, Volume2Icon } from "lucide-react";
 
 export const Route = createFileRoute("/dashboard/status/")({
   component: RouteComponent,
@@ -10,7 +10,7 @@ export const Route = createFileRoute("/dashboard/status/")({
 function RouteComponent() {
   return (
     <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-      <GlassCard className="aspect-square col-span-2">
+      <GlassCard className="aspect-square col-span-2 row-span-2">
         <div className="flex flex-col items-center justify-center h-full">
           <Button
             isIconOnly
@@ -27,61 +27,26 @@ function RouteComponent() {
           </div>
         </div>
       </GlassCard>
-      <Card className="p-6 col-span-2 md:col-span-1">
-        <Card.Title>Welcome to your dashboard</Card.Title>
-        <Card.Content>
-          This is the main area where you can view and manage your data.
-        </Card.Content>
-      </Card>
-      <GlassCard className="aspect-square col-span-2">
-        <div className="flex flex-col items-center justify-center h-full">
-          <Button
-            isIconOnly
-            variant="secondary"
-            className="w-40 h-40 rounded-full border"
-          >
-            <Volume2Icon className="w-16 h-16" />
-          </Button>
-          <div className="flex flex-col items-center mt-10 text-center space-y-4">
-            <h3 className="text-2xl">Localizar BIA-RADAR</h3>
-            <p className="text-muted">
-              Use este botão para emitir um sinal sonoro no dispositivo.
-            </p>
+
+      <GlassCard className="col-span-2 row-span-1 w-full h-full">
+        <div className="flex w-full justify-between items-center p-4 gap-4">
+          <div className="bg-surface-secondary p-4 rounded-4xl text-muted">
+            <CloudDownloadIcon />
           </div>
+
+          <div className="w-full">
+            <h3>Atualização de Software</h3>
+            <p className="text-muted text-sm uppercase tracking-widest">V2.4.1 Disponível</p>
+          </div>
+          <ChevronRightIcon className="text-muted"/>
         </div>
       </GlassCard>
-      <GlassCard className="aspect-square col-span-2">
-        <div className="flex flex-col items-center justify-center h-full">
-          <Button
-            isIconOnly
-            variant="secondary"
-            className="w-40 h-40 rounded-full border"
-          >
-            <Volume2Icon className="w-16 h-16" />
-          </Button>
-          <div className="flex flex-col items-center mt-10 text-center space-y-4">
-            <h3 className="text-2xl">Localizar BIA-RADAR</h3>
-            <p className="text-muted">
-              Use este botão para emitir um sinal sonoro no dispositivo.
-            </p>
-          </div>
-        </div>
-      </GlassCard>
-      <GlassCard className="aspect-square col-span-2">
-        <div className="flex flex-col items-center justify-center h-full">
-          <Button
-            isIconOnly
-            variant="secondary"
-            className="w-40 h-40 rounded-full border"
-          >
-            <Volume2Icon className="w-16 h-16" />
-          </Button>
-          <div className="flex flex-col items-center mt-10 text-center space-y-4">
-            <h3 className="text-2xl">Localizar BIA-RADAR</h3>
-            <p className="text-muted">
-              Use este botão para emitir um sinal sonoro no dispositivo.
-            </p>
-          </div>
+      <GlassCard className="col-span-1 row-span-1 aspect-square">
+        <div className="flex flex-col p-4 gap-4 h-full justify-center items-center">
+          <BatteryIcon className="text-accent w-10 h-10"/>
+          <p className="text-2xl">
+            84%
+          </p>
         </div>
       </GlassCard>
     </div>
